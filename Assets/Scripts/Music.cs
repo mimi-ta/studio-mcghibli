70500d9fd80e5284792094cbd873e7868e3044c2
marcusphan <marcus.phan.1@ens.etsmtl.ca>
2021-01-17 11:38:38 -0500

    Music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+}
