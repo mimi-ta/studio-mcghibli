bc10cc71e895a37d1978f3a32a959e7e848285a3
marcusphan <marcus.phan.1@ens.etsmtl.ca>
2021-01-16 20:22:54 -0500

    Collision work

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+{
+    public string NextSceneName;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(NextSceneName);
+    }
+}
