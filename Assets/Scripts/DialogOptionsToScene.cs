2ab53841a96901a9812f4f6578103c4bc94b4ef8
Jason Beccherini <jason.beccherini@gmail.com>
2021-01-17 09:21:42 -0500

    implmented clicking decision to go to next scene

--- Assets/Scripts/DialogOptionsToScene.cs
+++ Assets/Scripts/DialogOptionsToScene.cs
@@ -17,13 +17,84 @@ public class DialogOptionsToScene : MonoBehaviour
         
     }
 
-    void option4a()
+    public void option4a()
     {
         SceneManager.LoadScene(sceneName: "5");
     }
-    void option4b()
+    public void option4b()
     {
         SceneManager.LoadScene(sceneName: "31");
+    }
+
+    public void option5a()
+    {
+        SceneManager.LoadScene(sceneName: "6");
+    }
+    public void option5b()
+    {
+        SceneManager.LoadScene(sceneName: "20");
+    }
+
+    public void option6a()
+    {
+        SceneManager.LoadScene(sceneName: "7");
+    }
+    public void option6b()
+    {
+        SceneManager.LoadScene(sceneName: "8");
+    }
+    public void option6c()
+    {
+        SceneManager.LoadScene(sceneName: "14");
+    }
+    public void option9a()
+    {
+        SceneManager.LoadScene(sceneName: "10");
+    }
+    public void option9b()
+    {
+        SceneManager.LoadScene(sceneName: "12");
+    }
+    public void option14a()
+    {
+        SceneManager.LoadScene(sceneName: "15");
+    }
+    public void option14b()
+    {
+        SceneManager.LoadScene(sceneName: "17");
+    }
+    public void option20a()
+    {
+        SceneManager.LoadScene(sceneName: "21");
+    }
+    public void option20b()
+    {
+        SceneManager.LoadScene(sceneName: "29");
+    }
+
+    public void option21a()
+    {
+        SceneManager.LoadScene(sceneName: "22");
+    }
+    public void option21b()
+    {
+        SceneManager.LoadScene(sceneName: "24");
+    }
+    public void option24a()
+    {
+        SceneManager.LoadScene(sceneName: "25");
+    }
+    public void option24b()
+    {
+        SceneManager.LoadScene(sceneName: "27");
+    }
+    public void option32a()
+    {
+        SceneManager.LoadScene(sceneName: "33");
+    }
+    public void option32b()
+    {
+        SceneManager.LoadScene(sceneName: "35");
     }
 
 
