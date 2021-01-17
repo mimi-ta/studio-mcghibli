91f43aa58e988f735eeb1205d1373b4e35da9cbf
marcusphan <marcus.phan.1@ens.etsmtl.ca>
2021-01-17 04:54:06 -0500

    death

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (!FindObjectOfType<DialogueManager>().animator.GetBool("isOpen"))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
